a03870c88b0a2f3069b2c1d5e7e8702b6fab376f
kollyaz <117323099+kollyaz@users.noreply.github.com>
2026-01-17 13:02:10 +0100

    Add files via upload

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -59,7 +59,7 @@ export function MovieDetails({
     async function getMovieDetails() {
       setLoading(true);
       const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
       );
       const data = await res.json();
       setMovie(data);
